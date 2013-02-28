5aba226ae625a9adc1a4bc0c5936110234bbb74e
gparlakov <gparlakov@gmail.com>
2013-02-28 13:28:03 -0800

    In 2.OOP Defining classes Added struct Point3D.cs and overrided ToString()..

---
+++ 2.DefiningClasses_PartII/1.Point3D/Point3D.cs
@@ -0,0 +1,45 @@
+﻿
+
+using System;
+using System.Linq;
+
+namespace First.Point3D
+{
+    public struct Point3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        static readonly Point3D center;
+        
+        
+        private static Point3D Center
+        {
+            get
+            {
+                return new Point3D(0, 0, 0);
+            }
+            set
+            {
+                //this.center = value;
+            }
+        }
+
+        //public Point3D()
+        //{
+        //    this.Center = new Point3D { 0, 0, 0 };
+        //}
+
+        public Point3D(int x, int y, int z)           
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Point[x,y,z]: [{0},{1},{2}]",  this.X, this.Y, this.Z);
+        }
+    }
+}
---
+++ 2.DefiningClasses_PartII/1.Point3D/TestPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First.Point3D
+{
+    class TestPoint
+    {
+        static void Main()
+        {
+            Point3D point1 = new Point3D(1, 1, 1);
+            Console.WriteLine(point1);
+
+        }
+    }
+}
