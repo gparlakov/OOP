8b156837495abb60646360fc212c297faa107838
gparlakov <gparlakov@gmail.com>
2013-03-01 03:27:35 -0800

    Some strange change in 1.DefiningClasss.... suo? Added static field center and property for it.Added method to calculate 3D distance DistanceCalculator3D.cs

--- 2.DefiningClasses_PartII/1.Point3D/Point3D.cs
+++ 2.DefiningClasses_PartII/1.Point3D/Point3D.cs
@@ -3,43 +3,54 @@
 using System;
 using System.Linq;
 
-namespace First.Point3D
+namespace ThreeDimensionalSpace
 {
     public struct Point3D
     {
+        private string name;
         public int X { get; set; }
         public int Y { get; set; }
         public int Z { get; set; }
-        static readonly Point3D center;
-        
-        
-        private static Point3D Center
+        public string Name
         {
             get
             {
-                return new Point3D(0, 0, 0);
+                return this.name;
             }
             set
             {
-                //this.center = value;
+                if (String.IsNullOrEmpty(value))
+                {
+                    this.name = String.Format("[{0},{1},{2}]", this.X, this.Y, this.Z);
+                }
+                else
+                {
+                    this.name = value;
+                }
             }
         }
+        private static readonly Point3D center = new Point3D(0, 0, 0);
 
-        //public Point3D()
-        //{
-        //    this.Center = new Point3D { 0, 0, 0 };
-        //}
-
-        public Point3D(int x, int y, int z)           
+        public Point3D(int x, int y, int z,string name=null)    
+            :this()
         {
             this.X = x;
             this.Y = y;
             this.Z = z;
+            this.Name = name;
+        }
+
+        public static Point3D Center
+        {
+            get
+            {
+                return center;
+            }
         }
 
         public override string ToString()
         {
-            return String.Format("Point[x,y,z]: [{0},{1},{2}]",  this.X, this.Y, this.Z);
+            return String.Format("Point[{3}]: [{0},{1},{2}]",  this.X, this.Y, this.Z,this.Name);
         }
     }
 }
--- 2.DefiningClasses_PartII/1.Point3D/TestPoint.cs
+++ 2.DefiningClasses_PartII/1.Point3D/TestPoint.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*1.Create a structure Point3D to hold a 3D-coordinate {X, Y, Z} in the Euclidian 3D space.  * Implement the ToString() to enable printing a 3D point.2.Add a private static read-only field to hold the start of the coordinate  * system – the point O{0, 0, 0}. Add a static property to return the point O.3.Write a static class with a static method to calculate the distance between * two points in the 3D space.4.Create a class Path to hold a sequence of points in the 3D space. Create a 
+ * static class PathStorage with static methods to save and load paths from a
+ * text file. Use a file format of your choice.
+*/
+using System;
 
-namespace First.Point3D
+namespace ThreeDimensionalSpace
 {
     class TestPoint
     {
         static void Main()
         {
-            Point3D point1 = new Point3D(1, 1, 1);
-            Console.WriteLine(point1);
-
+            Point3D point1 = new Point3D(-1, -1, 1,"K");
+            Point3D point2 = new Point3D(-2, -2, -2, "M");           
+            Console.WriteLine("Center {0}\n{1}\n{2}\nDistance between point {4} and {5}:\t{3}", Point3D.Center, point1, point2, 
+                DistanceCalculator3D.CalculateDistance3D(point1, point2),point1.Name,point2.Name);          
         }
     }
 }
