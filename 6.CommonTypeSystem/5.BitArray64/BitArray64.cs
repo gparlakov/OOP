87379a9437792bc9c712b81598431338e6526f62
gparlakov <gparlakov@gmail.com>
2013-03-17 09:29:48 -0700

    Last two commits were to add some homework for the CTS HomeWork. As is this one

--- 6.CommonTypeSystem/5.BitArray64/BitArray64.cs
+++ 6.CommonTypeSystem/5.BitArray64/BitArray64.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BitArray64
 {
-    class BitArray64
+    class BitArray64 : IEnumerable<int>
     {
         private ulong bitValue;
 
@@ -15,6 +15,21 @@ namespace BitArray64
             get { return bitValue; }
             set { bitValue = value; }
         }
-        
+
+        public BitArray64(ulong value)
+        {
+            this.bitValue = value;
+        }
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
